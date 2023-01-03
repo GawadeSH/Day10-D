08e2a635be240f7460976f55f9d3b87babb446c7
shital gawade <shitalgawade131@gmail.com>
2023-01-03 11:51:32 +0530

    percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-    flip=$(($RANDOM%2))
+flip=$(($RANDOM%2))
     if [[  flip -eq 1 ]]
     then
          echo "head"
-    else 
+    elif [[ flip -eq 0 ]]
+    then
          echo "tail"
      fi
+
+count1=0
+count2=0
+for((i=0;i<=100;i++))
+do
+
+    flip=$(($RANDOM%2))
+    if [[  flip -eq 1 ]]
+    then
+        
+         ((count1++))
+
+    elif [[ flip -eq 0 ]]
+    then
+         ((count2++))
+     fi
+done
+percent1=($count1/100)
+echo "$percent1"
+percent2=($count2/100)
+echo "$percent2"
